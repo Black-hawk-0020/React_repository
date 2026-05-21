8ccd716d51e898d9f637f52d83817b9a94be1fde
Black-hawk-0020 <valentyn0020@gmail.com>
2026-05-21 12:55:37 +0300

    add #Fs05Kiho

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,24 @@
 import "./App.css";
+import type {Icourse} from "./modalCourse";
+const coursesAndDurationArray: Icourse[]= [
+  { title: "JavaScript Complex", monthDuration: 5 },
+  { title: "Java Complex", monthDuration: 6 },
+  { title: "Python Complex", monthDuration: 6 },
+  { title: "QA Complex", monthDuration: 4 },
+  { title: "FullStack", monthDuration: 7 },
+  { title: "Frontend", monthDuration: 4 },
+];
 
 function App() {
-  return <></>;
+  return (
+    <ul>
+      {coursesAndDurationArray.map(({ title, monthDuration }, index) => (
+        <li key={index}>
+          {title}-{monthDuration}
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
