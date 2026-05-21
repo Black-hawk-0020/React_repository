fd477a307bbd5453fcfc38c0329a8b06e45d5de4
Black-hawk-0020 <valentyn0020@gmail.com>
2026-05-21 11:55:51 +0300

    #gi01MZ6v

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,7 +1,21 @@
 import "./App.css";
+const coursesTitleArray: string[] = [
+  "JavaScript Complex",
+  "Java Complex",
+  "Python Complex",
+  "QA Complex",
+  "Fullstack",
+  "Frontend",
+];
 
 function App() {
-  return <></>;
+  return (
+    <ul>
+      {coursesTitleArray.map((value, index) => (
+        <li key={index}>{value}</li>
+      ))}
+    </ul>
+  );
 }
 
 export default App;
